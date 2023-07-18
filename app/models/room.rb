b3725595994ebbe426b49d281083ee2ccb73bc11
kevin623Wiz <tamagoball10@gmail.com>
2023-07-19 00:06:55 +0900

    Roomのバリデーションの追加、フォームの整備

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,10 @@
 class Room < ApplicationRecord
     mount_uploader :room_image, RoomImageUploader
     belongs_to :user
+
+    validates :name, presence: true
+    validates :introduction, presence: true
+    validates :price, presence: true
+    validates :address, presence: true
+
 end
